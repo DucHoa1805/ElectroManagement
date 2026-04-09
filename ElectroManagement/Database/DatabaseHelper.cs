51465e7e728b111c976aed4f2fb8540ee69a0e3f
Duc Hoa <duchoa0741@gmail.com>
2026-04-10 01:19:07 +0700

    Add project files.

---
+++ ElectroManagement/Database/DatabaseHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace ElectroManagement.Database
+{
+    public class DatabaseHelper
+    {
+        private static string connectionString =
+            "Data Source=localhost;Initial Catalog=ElectroDB;Integrated Security=True";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
