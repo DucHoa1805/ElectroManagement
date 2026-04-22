187a8165a5bce34e305f2ad594786c3e75189ff5
Ninh <email_cua_ban@gmail.com>
2026-04-22 15:07:44 +0700

    update inventory

--- ElectroManagement/Program.cs
+++ ElectroManagement/Program.cs
@@ -16,7 +16,7 @@ namespace ElectroManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Views.frmMain());
+            Application.Run(new Views.Inventory.frmInventory());
         }
     }
 }
